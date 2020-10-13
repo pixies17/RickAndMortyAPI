de1fc5133af59e61cb49b80207edd2611d81ae22
Egor Kosilov <e.kosilov@napoleonit.ru>
2020-10-13 15:35:33 +0500

    create a proj and add snapkit

---
+++ RickAndMortyAPI/RickAndMortyAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyAPI
+//
+//  Created by Egor Kosilov on 13.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
